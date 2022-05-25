295f9c2a84fd3c2ef5a8cb5a3394cdd9f65ef6b9
Chris Podivinsky <chriswebst@protonmail.com>
2022-05-25 12:02:38 +0200

    Update task_13.c

--- 10_22_more_advanced/task_13.c
+++ 10_22_more_advanced/task_13.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include <math.h>
 
-/*
-    #13
-    Load the number n <int>. 
-    Display the count of the preview numbers and n <int>.
-*/
+/**
+ * #13
+ * Load the number n <int>.
+ * Display the count of the preview numbers and n <int>.
+ **/
 
 int main()
 {   int again;
@@ -15,20 +15,29 @@ int main()
     int count;  /* defines the temporary count */
     
     do{
-        printf("Please enter the number n to compute the count of its precedessors and it <int>.\n"); /* prints a line describing the task for the user */
-        scanf("%i", &n);  /* loads n */
+        printf("\nPlease enter the number n to compute the count first n ints <int>.\n");
+        scanf("%i", &n);
         count = 0;
         
-        for (for_n = 0; for_n <= n; for_n++)  /* raises for_n by one as long as it is equal or lower than n */
-            count = count + for_n;  /* The count is equal to count from the previous iteration enlarged by the actual for_n */
-        
-        again = 0;
-        printf("The count is equal to %i. Type 1 if you wanna countinue with another n, type something diffirent and the programme will be finished.", count);
+        for (for_n = 1; for_n <= n; for_n++){
+
+            /**
+             * The count is equal to count from the previous iteration 
+             * enlarged by the actual for_n 
+            **/
+            count = count + for_n;
+
+        }
+
+        printf("The count is equal to %i.\n"
+        "Type 1 if you wanna countinue with another n, "
+        "type something diffirent and the programme will be finished.", count);
         scanf("%i", &again);
         
     
     }while(again == 1);
-    printf("That's all, thanks for using my calculator.");
+
+    printf("\nThat's all, thanks for using my calculator.");
     
  
     return 0;
