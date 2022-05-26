01be2a67c4510ef19443a38238c692da421f75b2
Chris Podivinsky <chriswebst@protonmail.com>
2022-05-26 12:32:32 +0200

    #17 added

---
+++ 10_22_more_advanced/task_17.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * #17
+ * Load a group of ints ending with zero.
+ * Display their arithmetic mean.
+ **/
+
+int main()
+{   
+    int again;
+    int number;
+    int counter;  /* Counts of the loaded numbers (with the ending zero). */
+    float armean;
+    
+    do{
+        armean = 0;  /* Needs to be set here for possible reuse. */
+        counter = 0;
+
+        printf("Please enter a group of ints ending with zero "
+        "to display their arithmetic mean.\n");
+        
+        do{
+
+            scanf("%i", &number);
+
+            armean = armean + number;
+            counter = counter + 1;
+
+        }while(number != 0);
+
+        armean = armean/(counter - 1);  /* Doesn't include the ending zero. */
+        
+        again = 0;
+        printf("The arithmetic mean is equal to %.2f.\n"
+        "Type 1 if you wanna countinue with another group of numbers, "
+        "type something diffirent and the programme will be finished.", armean);
+        scanf("%i", &again);
+        
+    
+    }while(again == 1);
+
+    printf("That's all, thanks for using my calculator.");
+ 
+    return 0;
+}
