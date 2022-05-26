51864e088da5d6cd95530c1177b8a6e6de1ade66
Chris Podivinsky <chriswebst@protonmail.com>
2022-05-26 12:19:17 +0200

    Add files via upload

---
+++ 10_22_more_advanced/task_16.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * #16
+ * Load a group of positive ints ending with zero.
+ * Display maximum of those numbers.
+ **/
+
+int main()
+{   
+    int again;
+    int number;
+    int maximum;
+    
+    do{
+        maximum = 0;  /* Needs to be set here for possible reuse. */
+
+        printf("Please enter a group of positive ints ending with zero "
+        " to display the maximum.\n");
+        
+        do{
+
+            scanf("%i", &number);
+
+            if(number > maximum){
+
+                maximum = number;
+
+            }
+
+        }while(number != 0);
+        
+        again = 0;
+        printf("The maximum is equal to %i.\n"
+        "Type 1 if you wanna countinue with another group of numbers, "
+        "type something diffirent and the programme will be finished.", maximum);
+        scanf("%i", &again);
+        
+    
+    }while(again == 1);
+
+    printf("That's all, thanks for using my calculator.");
+ 
+    return 0;
+}
