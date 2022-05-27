84c0642b1058132e02ce12cad9cc29e52dcf7c18
Chris Podivinsky <chriswebst@protonmail.com>
2022-05-27 11:38:08 +0200

    Update task_18.c (wrong assignment)

--- 10_22_more_advanced/task_18.c
+++ 10_22_more_advanced/task_18.c
@@ -4,32 +4,38 @@
 
 /*
     #18
-    Load the numbers <int> until the user enters 0. 
-    Display the count of all the submitted numbers <int>.
+    Load n <int> and display n! (n factorial).
  */
 
 int main()
-{   int again;
-    int n;  /* defines the number n */
-    int count;  /* defines the temporary count */
+{   
+    int again;
+    int iterator;
+    int n;
+    int factorial;
+
+   do{
+       
+        printf("\nPlease enter number n <int> do display n! (n factorial).\n");
+        scanf("%i", &n);
     
-    do{
-        count = 0;
-        do{
-            printf("Please enter a number you want to include in your count <int> If you type 0; the final count will be displayed.\n"); /* prints a line describing the task for the user */
-            scanf("%i", &n);  /* loads n */
-            count = count + n;
-        
-        }while(n != 0);
+        factorial = 1;
+
+        for(iterator = 1; iterator <= n; iterator++){
+
+            factorial = factorial * iterator;
+
+        }
         
         again = 0;
-        printf("The count is equal to %i. Type 1 if you wanna countinue with another n, type something diffirent and the programme will be finished.", count);
+        printf("n! is equal to %i.\n"
+        "Type 1 if you wanna countinue with another n, "
+        "type something else and the program will be finished.", factorial);
         scanf("%i", &again);
-        
     
     }while(again == 1);
+
     printf("That's all, thanks for using my calculator.");
-    
- 
+
     return 0;
 }
