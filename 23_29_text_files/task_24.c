d5180b57dabe986c81230d19af514f54e59e85c2
Chris Podivinsky <chriswebst@protonmail.com>
2022-05-29 00:35:03 +0200

    Add files via upload

---
+++ 23_29_text_files/task_24.c
@@ -0,0 +1,39 @@
+/**  
+ * #24
+ * Write a program which loads
+ * three ints from the file data.txt
+ * (each on a separate line)
+ * and prints their sum on the monitor  
+ **/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    int sum;
+    int i;
+    int number;
+    FILE *fileread;  /* Operating chart for the created (writing) file. */
+
+    sum = 0;
+
+    /**
+     * Loading of data.txt (type "read"),
+     * initiates operating with the filewrite chart.
+     **/
+    fileread = fopen("data.txt", "r");
+
+    for(i = 0; i < 3; i++){
+        
+        fscanf(fileread, "%i\n", &number);
+        sum = sum + number;
+
+    }
+
+    fclose(fileread);  /* Ends operating with the chart. */
+    
+    printf("Sum of the three numbers defined in data.txt is equal to %i.", sum);
+
+}
