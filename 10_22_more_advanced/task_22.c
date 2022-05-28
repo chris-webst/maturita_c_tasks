ea8ee3aea8ec114dfdc594dd788135bc046918af
Chris Podivinsky <chriswebst@protonmail.com>
2022-05-28 23:58:59 +0200

    #22 added

---
+++ 10_22_more_advanced/task_22.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * #22
+ * Display both the small and large
+ * multiplication table using cycles.
+ **/
+
+int main()
+{   
+    int i;
+    int j;
+
+    printf("Small multiplication table\n\n");
+
+    for(i = 1; i < 10; i++){
+
+        for(j = 1; j < 10; j++){
+
+            printf("%3i", i * j);
+
+        }
+        
+        printf("\n");
+        
+    }
+    
+    printf("\nLarge multiplication table\n\n");
+
+    for(i = 10; i < 20; i++){
+
+        for(j = 1; j < 10; j++){
+
+            printf("%4i", i * j);
+
+        }
+        
+        printf("\n");
+        
+    }
+
+    return 0;
+}
