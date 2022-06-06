9e53e7c488868336efdde0af6ea063d797cf3976
Chris Podivinsky <chriswebst@protonmail.com>
2022-06-06 22:09:33 +0200

    Update task_30.c

--- 30_35_functions/task_30.c
+++ 30_35_functions/task_30.c
@@ -14,6 +14,31 @@
 #include <stdlib.h>
 #include <string.h>
 
+int maximum(int first, int second, int third);
+
+
+int main()
+{
+    int first;
+    int second;
+    int third;
+    int i;
+    int maximums[3];
+
+    for(i = 0; i <3; i++){
+        
+        printf("\n%i. ROUND\nPlease enter three numbers <int> to compute their maximum.", i + 1);
+        scanf("%i %i %i", &first, &second, &third);
+
+        maximums[i] = maximum(first, second, third);
+    
+    }
+
+    printf("The final maximum is equal to %i.", maximum(maximums[0], maximums[1], maximums[2]));
+    
+    return 0;
+
+}
 
 int maximum(int first, int second, int third)
 {
@@ -44,28 +69,5 @@ int maximum(int first, int second, int third)
     }
 
     return maximum;
-
-}
-
-int main()
-{
-    int first;
-    int second;
-    int third;
-    int i;
-    int maximums[3];
-
-    for(i = 0; i <3; i++){
-        
-        printf("\n%i. ROUND\nPlease enter three numbers <int> to compute their maximum.", i + 1);
-        scanf("%i %i %i", &first, &second, &third);
-
-        maximums[i] = maximum(first, second, third);
-    
-    }
-
-    printf("The final maximum is equal to %i.", maximum(maximums[0], maximums[1], maximums[2]));
-    
-    return 0;
 
 }
