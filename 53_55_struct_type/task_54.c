e808ec09029a46ba989bbf9fe90d37d7b465a0c6
Chris Podivinsky <chriswebst@protonmail.com>
2022-06-07 23:45:34 +0200

    Update task_54.c

--- 53_55_struct_type/task_54.c
+++ 53_55_struct_type/task_54.c
@@ -1,13 +1,14 @@
-/*  
-    #54
-    IDs (int) and heights (float) of three students
-    are given by the user. Display the ID and height 
-    of the highest student.
-    Use struct type.
-*/
+/**
+ * #54
+ * IDs (int) and heights (float) of three students
+ * are given by the user. Display the ID and height 
+ * of the highest student.
+ * Use struct type.
+ **/
 
 #include <stdio.h>
 #include <stdlib.h>
+
 
 typedef struct{
 
@@ -16,11 +17,11 @@ typedef struct{
 
 }student;
 
-
 int main()
 {
-
-    student student1; student2; student3;
+    student student1; 
+    student student2; 
+    student student3;
     int maxid;
     float maxheight;
 
@@ -33,18 +34,28 @@ int main()
     printf("Please enter the ID and height of the third student.\n");
     scanf("%i %f", &student2.id, &student2.height);
 
-    // Most basic searching for maximum.
+    /** Most basic searching for maximum. **/
     maxid = student1.id;
     maxheight = student1.height;
 
-    if(student2.height > maxheight){
+    if(student2.height >= maxheight){
 
         maxheight = student2.height;
         maxid = student2.id;
 
     }
 
-    if (studen3.height > maxheight){
+    /**
+     * This condition is not an if–else condition
+     * which means that the program has to go
+     * through it no matter what.
+     * That solves the case of
+     * student1.height < student2.height < student3.height.
+     * 
+     * When two students have the same height,
+     * only one of them will be displayed.
+     **/
+    if (student3.height >= maxheight){
 
         maxheight = student3.height;
         maxid = student3.id; 
