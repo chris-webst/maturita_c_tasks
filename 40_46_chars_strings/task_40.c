7add337c6b9a723469714979bbb2a3c7cce79a04
Chris Podivinsky <chriswebst@protonmail.com>
2022-06-12 22:11:03 +0200

    Update task_40.c

--- 40_46_chars_strings/task_40.c
+++ 40_46_chars_strings/task_40.c
@@ -1,26 +1,36 @@
-/*  
-    #40
-    Read the text file data.dat line by line
-    and display on the the monitor with the
-    rule of maximal line length of 80.
-*/
+/**
+ * #40
+ * Read the text file data.dat line by line
+ * and display its contents on the the monitor
+ * with the maximal line length of 80.
+ **/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+
 int main()
 {
-    
-    char oneline[81];  // 80 chars plus \0
-    fr = fopen("data.dat", "r");  // Reading the text file.
-    // fgets() parameters are end variable, length, source file.
-    while(fgets(oneline, 80, fr) != NULL)  // Read until reaching the \0 character.
-    {
+    FILE *fileread;  /* Operating chart for the given (reading) file. */
+    char oneline[81];  /* 80 chars plus \0 */
 
-        printf("%s", oneline);  // Print the first 80 chars into one line.
+    fileread = fopen("data.dat", "r");  /* Reading the text file. */
+
+    /**
+     * fgets() will split the data.dat contents
+     * into lines of the maximal length of 80 characters.
+     * It will scan the file until reaching the \0 character.
+     * Its parameters are the end variable (one line of the
+     * final printed text), length of the line and the source file.
+     **/
+    while(fgets(oneline, 80, fileread) != NULL){
+
+        printf("%s\n", oneline);
 
     }
+    
+    fclose(fileread);
     
     return 0;
 
