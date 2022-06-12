db4462d5a2acebb538e9c86c7ff69e2155a04859
Chris Podivinsky <chriswebst@protonmail.com>
2022-06-12 22:26:19 +0200

    Update task_41.c

--- 40_46_chars_strings/task_41.c
+++ 40_46_chars_strings/task_41.c
@@ -1,24 +1,36 @@
-/*  
-    #41
-    Write a program that loads three strings
-    and displays them togehter as one string
-    with spaces on the monitor (strcat).
-*/
+/**
+ * #41
+ * Write a program that loads three strings
+ * and displays them togehter as one string
+ * with spaces on the monitor.
+ * 
+ * Use the strcat() fuction.
+ **/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+
 int main()
 {
-    char string_1[15];  // CAUTION! string_1 = "" without [] is prohibited
+    /**
+     * CAUTION! string_1 = "" without [] is prohibited.
+     * Another variable declaration option is
+     * string_1[] = {'H', 'e', 'l', 'l', 'o'}.
+     **/
+    char string_1[15];
     char string_2[15];
     char string_3[15];
     
-    int i;
+    printf("You're about to insert three strings you want to "
+    "connect together. Please keep in mind that you can't use "
+    "spaces nor enters within one string. If you need to insert "
+    "a string which consists of more than one word, use underscores.\n\n");
     
     printf("Insert 1. string:\n");
-    scanf("%s", string_1);  // CAUTION! scanf() doesn't use & for scanning strings
+    /* CAUTION! scanf() doesn't use & for scanning strings. */
+    scanf("%s", string_1);
     
     printf("\nInsert 2. string:\n");
     scanf("%s", string_2);
@@ -26,12 +38,13 @@ int main()
     printf("\nInsert 3. string:\n");
     scanf("%s", string_3);
 
-    strcat(string_1, " ");  // strcat() connects just two strings
+    strcat(string_1, " ");  /* CAUTION! strcat() connects just two strings. */
     strcat(string_1, string_2);
     strcat(string_1, " ");
     strcat(string_1, string_3);
 
-    // strcat() saves the connected string into the first variable
+    printf("\nThe final string:");
+    /* strcat() saves the connected string into the first variable. */
     printf("\n%s", string_1);
 
     return 0;
