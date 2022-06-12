2c81263af2df9362e8622473c9c08db6c5fefc32
Chris Podivinsky <chriswebst@protonmail.com>
2022-06-12 22:40:16 +0200

    Update task_42.c

--- 40_46_chars_strings/task_42.c
+++ 40_46_chars_strings/task_42.c
@@ -1,45 +1,49 @@
-/*  
-    #42
-    There are two strings of maximal size of 20 chars given.
-    Compare them (strcomp) and if they're identical, 
-    display their length (strlen).
-    If they're not identical, separately display their length.
-*/
+/**
+ * #42
+ * There are two strings of maximal size
+ * of 10 chars given by the user.
+ * Compare them using the strcmp() function
+ * and if they're identical, display their length –
+ * for that, use the strlen() function.
+ * If they're not identical, display their length
+ * separately.
+ **/
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>  // This is the library with string support.
+#include <string.h> 
+
 
 int main()
 {
+    /* Size of these strings is 10 and not 11; last char is \0 for comparison. */
+    char string1[11];
+    char string2[11];
 
-    // Size of these strings is 10 and not 11; last char is \0 for comparison.
-    char string1[11], string2[11];
-
-    printf("\nInsert first string (maximal size is 10)\n");
+    printf("\nInsert first string (maximal size is 10; no enters or spaces).\n");
     scanf("%s", string1);
-    printf("\nInsert first string (maximal size is 10)\n");
+
+    printf("\nInsert second string (maximal size is 10; no enters or spaces).\n");
     scanf("%s", string2);
 
-    if (strcmp(string1, string2) == 0)   // strcmp() displays 0 if strings are identical.
-    {
+    /* strcmp() displays 0 if the strings are identical. */
+    if (strcmp(string1, string2) == 0){
 
-        printf("These strings are identical.\n");
-
-        // There's no need for definition of %i variable, strlen() gives always int values.
-        printf("Their length is %i chars.\n", strlen(string1));
+        printf("These strings are identical.\n"
+        "Their length is %i chars.\n", strlen(string1));
 
     }
 
     else
     {
 
-        printf("These strings are not identical.\n");
-        printf("Length of the first string is %i chars.\n", strlen(string1));
-        printf("Length of the second string is %i chars.\n", strlen(string2));
+        printf("These strings are not identical.\n"
+        "Length of the first string is %i char(s).\n"
+        "Length of the second string is %i char(s).\n",
+        strlen(string1), strlen(string2));
 
     }
-    
+
     return 0;
-    
+
 }
