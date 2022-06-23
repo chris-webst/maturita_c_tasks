49149a0724e0994412c189baa6b6185299101652
Chris Podivinsky <chriswebst@protonmail.com>
2022-06-23 23:50:59 +0200

    Update task_44.c

--- 40_46_chars_strings/task_44.c
+++ 40_46_chars_strings/task_44.c
@@ -1,31 +1,38 @@
-/*  
-    #44
-    Write a program that converts a character
-    to an ASCII variable and conversely.
-*/
+/**
+ * #44
+ * Write a program that converts a character
+ * to an ASCII variable and conversely.
+ **/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+
 int main()
 {
-
-    char character;  // CAUTION! Use '' for chars and "" (+ space for \0) for strings.
+    /**
+     * CAUTION! Use '' for chars and ""
+     * (+ space for \0) for strings.
+     **/
+    char character;
     int integer;
     
-    // Conversion from ASCII to integer value.
+    /** Conversion from ASCII to integer value. **/
     printf("Insert any ASCII variable to be converted into decimal system.\n");
-    scanf("%c", &character);  // It's enough to convert the ASCII variable it to an integer.
-    integer = (int)character;  // Conversion into decimal system.
-    printf("\nThe ASCII variable %c has an integer value %i.\n", character, integer);
+    scanf("%c", &character);
+    /** Conversion into decimal system. **/
+    integer = (int)character;
+    printf("\nThe ASCII variable %c has an integer value %i.\n",
+    character, integer);
 
-    // Conversion from an integer value to ASCII.
+    /** Conversion from an integer value to ASCII. **/
     printf("\nInsert any integer.\n");
     scanf("%i", &integer);
-    character = (char)integer;  // Conversion into ASCII.
+    /** Conversion into ASCII. **/
+    character = (char)integer;
     printf("\nInteger %i has an ASCII variable %c.", integer, character);
-    
+
     return 0;
 
 }
