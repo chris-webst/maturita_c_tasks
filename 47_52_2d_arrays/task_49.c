2a2d0c56963327fc3997c82a87d1dc9fc0319b94
Chris Podivinsky <chriswebst@protonmail.com>
2022-07-15 12:09:30 +0200

    Update task_49.c

--- 47_52_2d_arrays/task_49.c
+++ 47_52_2d_arrays/task_49.c
@@ -1,31 +1,37 @@
-/*  
-    #49
-    Write a program which loads a 2D array with ints 
-    inserted by the user (5 rows, 4 columns)
-    and prints out the sum of them.
-*/
+/**
+ * #49
+ * Write a program which loads a 2D array with ints 
+ * inserted by the user (5 rows, 4 columns)
+ * and prints out the sum of them.
+ **/
 
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main()
 {
-    /* need two for loops --> 2D */
+    int array[5][4];
+    int i;
+    int j;
+    int sum;
 
-    int a[5][4], i, j, S;  /* rows, columns, iterators, sum */
+    printf("Insert 20 ints separated by Enter\n");
 
-    printf("Insert 20 ints separated by Enter or Space\n");
+    sum = 0;
 
-    S = 0;
+    for(i = 0; i <= 4; i++){
 
-    for(i = 0; i <= 4; i++)
-        for(j = 0; j <= 3; j++)
-        {
-            scanf("%i", &a[i][j]);
-            S = S + a[i][j];
+        for(j = 0; j <= 3; j++){
+
+            scanf("%i", &array[i][j]);
+            sum = sum + array[i][j];
+
         }
+    }
     
-    printf("Sum = %i\n", S);
+    printf("Sum = %i\n", sum);
 
     return 0;
+
 }
