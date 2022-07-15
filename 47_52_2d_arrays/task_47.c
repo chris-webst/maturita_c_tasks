bfa3ecaae0bbfac063d33de7f02b991d5555b18f
Chris Podivinsky <chriswebst@protonmail.com>
2022-07-15 11:53:52 +0200

    Update task_47.c

--- 47_52_2d_arrays/task_47.c
+++ 47_52_2d_arrays/task_47.c
@@ -1,32 +1,47 @@
-/*  
-    #47
-    Write a program that fills a two-dimensional array 
-    (5 rows and 4 columns) with integer values entered 
-    from the keyboard and writes the largest element 
-    of this array to the monitor.
-*/
+/**
+ * #47
+ * Write a program that fills a two-dimensional array 
+ * (5 rows and 4 columns) with integer values entered 
+ * from the keyboard and writes the largest element 
+ * of this array to the monitor.
+ **/
 
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main()
 {
-    int array[5][4], i, j, max;  /* array[rows][columns], iterators, maximum */
+    int array[5][4];  /* array[rows][columns] */
+    int i;
+    int j;
+    int max;
 
     printf("Insert 20 ints separated by Enter\n");
 
-    max = 0;  /* for the case of having at least one int > 0 */
+    for(i = 0; i <= 4; i++){
 
-    for(i = 0; i <= 4; i++)
-        for(j = 0; j <= 3; j++)
-        {
+        for(j = 0; j <= 3; j++){
+
             scanf("%i", &array[i][j]);
-            if (array[i][j] > max){
+
+            if(i == 0 && j == 0){
+
                 max = array[i][j];
-            };
+
+            }
+
+            else if (array[i][j] > max){
+
+                max = array[i][j];
+
+            }
+            
         }
-    
-    printf("Maximum of this array is: %i", max);
+    }
+
+    printf("Maximum of this array is %i.", max);
 
     return 0;
+
 }
